8393de71d04091733768952ec287e1f9559a5d1f
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-11 02:52:19 +0900

    feat: 시큐리티 설정 파일 작성

---
+++ src/main/java/org/bootstrap/apicomposer/global/security/WebFluxSecurityConfig.java
@@ -0,0 +1,84 @@
+package org.bootstrap.apicomposer.global.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bootstrap.apicomposer.global.exception.custom.UnAuthenticationException;
+import org.bootstrap.apicomposer.global.jwt.JwtProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Configuration
+@EnableWebFluxSecurity
+@Slf4j
+@RequiredArgsConstructor
+public class WebFluxSecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    private AuthenticationWebFilter authenticationWebFilter() {
+        ReactiveAuthenticationManager authenticationManager = Mono::just;
+
+        AuthenticationWebFilter authenticationWebFilter
+                = new AuthenticationWebFilter(authenticationManager);
+        authenticationWebFilter.setServerAuthenticationConverter(serverAuthenticationConverter());
+        return authenticationWebFilter;
+    }
+
+    private ServerAuthenticationConverter serverAuthenticationConverter(){
+        return exchange -> {
+            String token = jwtProvider.resolveToken(exchange.getRequest());
+            try {
+                if(!Objects.isNull(token) && jwtProvider.validateAccessToken(token)){
+                    return Mono.justOrEmpty(jwtProvider.getAuthentication(token));
+                }
+            } catch (UnAuthenticationException e) {
+                log.error(e.getMessage(), e);
+            }
+            return Mono.empty();
+        };
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges ->
+                        exchanges
+                                .pathMatchers("/api/test").authenticated()
+                                .anyExchange().permitAll()
+                )
+                .addFilterBefore(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
+                .httpBasic(Customizer.withDefaults())
+        ;
+        return http.build();
+    }
+
+    @Bean
+    MapReactiveUserDetailsService userDetailsService() {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        UserDetails rob = userBuilder.username("rob")
+                .password("rob")
+                .roles("USER")
+                .build();
+        UserDetails admin = userBuilder.username("admin")
+                .password("admin")
+                .roles("USER","ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(rob, admin);
+    }
+}
