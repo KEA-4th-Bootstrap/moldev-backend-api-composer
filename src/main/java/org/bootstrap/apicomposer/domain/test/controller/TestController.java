09ba2c0662b11ea2f2eafc2cbbb8e50ea29fb7dd
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-11 02:51:17 +0900

    feat: 테스트 로그인 기능 작성

--- src/main/java/org/bootstrap/apicomposer/domain/test/controller/TestController.java
+++ src/main/java/org/bootstrap/apicomposer/domain/test/controller/TestController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.bootstrap.apicomposer.domain.test.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -17,5 +18,18 @@ public class TestController {
     @GetMapping
     public Mono<String> test() {
         return Mono.just(testService.webClientTest());
+    }
+
+    @GetMapping("/free")
+    public Mono<String> noAuthTest() {
+        return Mono.just(testService.webClientTest());
+    }
+
+    @GetMapping("/login")
+    public Mono<String> testLogin(
+            @RequestParam String email,
+            @RequestParam String password
+    ) {
+        return Mono.just(testService.login(email, password));
     }
 }
