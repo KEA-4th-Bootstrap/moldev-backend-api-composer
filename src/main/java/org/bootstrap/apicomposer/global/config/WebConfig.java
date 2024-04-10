2c1990b1fe6882d3e70507c58d03daa670f9f890
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-11 02:52:03 +0900

    feat: CORS 매핑

---
+++ src/main/java/org/bootstrap/apicomposer/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.bootstrap.apicomposer.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://domain2.com")
+                .allowedMethods("PUT", "DELETE")
+                .allowCredentials(true).maxAge(3600);
+
+    }
+}
