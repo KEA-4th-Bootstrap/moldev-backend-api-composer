79acc5480c2c3c8703cc0b6ad95301bffa4783b4
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-11 11:56:25 +0900

    fix: test 메서드에 @UserId 테스트 추가

--- src/main/java/org/bootstrap/apicomposer/domain/test/controller/TestController.java
+++ src/main/java/org/bootstrap/apicomposer/domain/test/controller/TestController.java
@@ -1,7 +1,10 @@
 package org.bootstrap.apicomposer.domain.test.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bootstrap.apicomposer.domain.test.service.TestService;
+import org.bootstrap.apicomposer.global.common.auth.UserId;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,12 +14,16 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/test")
+@Slf4j
 public class TestController {
 
     private final TestService testService;
 
     @GetMapping
-    public Mono<String> test() {
+    public Mono<String> test(
+            @UserId Long userId
+            ) {
+        log.info("userId: {}", userId);
         return Mono.just(testService.webClientTest());
     }
 
