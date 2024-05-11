48d62160ca68d4d5addf215a28c83d3ac71f5144
OJOJIN <ojy101039@gmail.com>
2024-05-12 02:15:11 +0900

    KBB-65 [Feat] MSA 도메인별 URL 담는 Constant 추가

---
+++ src/main/java/org/bootstrap/apicomposer/global/common/Constants.java
@@ -0,0 +1,12 @@
+package org.bootstrap.apicomposer.global.common;
+
+public class Constants {
+    public static final String POST_SERVICE_URL = "http://localhost:8081";
+    public static final String USER_SERVICE_URL = "http://localhost:8082";
+    public static final String REPLY_SERVICE_URL = "http://localhost:8083";
+    public static final String SEARCH_SERVICE_URL = "http://localhost:8084";
+    public static final String AUTH_SERVICE_URL = "http://localhost:8085";
+    public static final String REPORT_SERVICE_URL = "http://localhost:8086";
+    public static final String CHATBOT_SERVICE_URL = "http://localhost:8087";
+    public static final String RECOMMEND_SERVICE_URL = "http://localhost:8088";
+}
