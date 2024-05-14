b6e659dd10a0b0aaafd4a0f2770a8a5ab4eba289
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-14 09:20:32 +0900

    feat: 기본 api 응답 클래스

---
+++ src/main/java/org/bootstrap/apicomposer/global/webclient/response/ApiResponse.java
@@ -0,0 +1,20 @@
+package org.bootstrap.apicomposer.global.webclient.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder(access = lombok.AccessLevel.PRIVATE)
+@Getter
+public class ApiResponse<T> {
+    private final int code;
+    private final String message;
+    private final T data;
+
+    public static <T> ApiResponse<?> of(ResponseCode responseCode, T data) {
+        return ApiResponse.builder()
+                .code(responseCode.getCode())
+                .message(responseCode.getMessage())
+                .data(data)
+                .build();
+    }
+}
---
+++ src/main/java/org/bootstrap/apicomposer/global/webclient/response/SuccessCode.java
@@ -0,0 +1,16 @@
+package org.bootstrap.apicomposer.global.webclient.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessCode implements ResponseCode {
+
+    // 2xx Success
+    SUCCESS(200, "요청에 성공하였습니다."),
+    CREATED(201, "생성되었습니다.");
+
+    private final int code;
+    private final String message;
+}
