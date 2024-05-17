61374372ab12b32316e9cfbb0172e520961c2541
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-18 01:00:11 +0900

    KBB-68 [Fix] post mapper 미적용

--- src/main/java/org/bootstrap/apicomposer/domain/post/service/PostService.java
+++ src/main/java/org/bootstrap/apicomposer/domain/post/service/PostService.java
@@ -1,9 +1,9 @@
 package org.bootstrap.apicomposer.domain.post.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bootstrap.apicomposer.domain.post.dto.response.*;
 import org.bootstrap.apicomposer.domain.post.helper.PostHelper;
-import org.bootstrap.apicomposer.domain.post.mapper.PostMapper;
 import org.bootstrap.apicomposer.domain.post.type.CategoryType;
 import org.bootstrap.apicomposer.domain.post.vo.CategoryPostVo;
 import org.bootstrap.apicomposer.domain.post.vo.SearchPostVo;
@@ -12,6 +12,9 @@ import org.bootstrap.apicomposer.domain.reply.helper.ReplyHelper;
 import org.bootstrap.apicomposer.domain.user.dto.response.UserDetailListResponseDto;
 import org.bootstrap.apicomposer.domain.user.dto.response.UserDetailResponseDto;
 import org.bootstrap.apicomposer.domain.user.helper.UserHelper;
+import org.bootstrap.apicomposer.domain.user.vo.UserProfileVo;
+import org.bootstrap.apicomposer.global.common.SuccessResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -19,47 +22,51 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PostService {
     private final PostHelper postHelper;
-    private final PostMapper postMapper;
     private final UserHelper userHelper;
     private final ReplyHelper replyHelper;
 
-    public Mono<PostCategoryResponseDto> getCategoryPost(String moldevId,
-                                                         CategoryType type,
-                                                         ServerHttpRequest request) {
-        Mono<CategoryPostVo> getCategoryPost = postHelper.getCategoryPost(moldevId, request.getHeaders());
-        Mono<UserDetailResponseDto> userDetailResponseDtoMono = userHelper.getUserProfileVo(moldevId, request.getHeaders());
-        return Mono.zip(getCategoryPost, userDetailResponseDtoMono).flatMap(tuple ->
-                Mono.just(postMapper.toPostCategoryResponseDto(tuple.getT1(), tuple.getT2()))
-        );
-    }
-
-    public Mono<SearchPostsResponseDto> getSearchPosts(String text, ServerHttpRequest request) {
-        Mono<PostDetailListResponseDto> searchPostVoMono = postHelper.getSearchPostResult(text, request.getHeaders());
-        return searchPostVoMono.flatMap(result -> {
-            List<Long> requestMembers = result.postList().stream()
-                    .map(SearchPostVo::memberId)
-                    .collect(Collectors.toList());
-            Mono<UserDetailListResponseDto> searchUserVoMono = userHelper.getSearchUserResult(requestMembers, request.getHeaders());
-            return searchUserVoMono.map(nextResult -> SearchPostsResponseDto.of(result, nextResult));
+    public Mono<ResponseEntity<SuccessResponse<?>>> getCategoryPost(String moldevId,
+                                                                    CategoryType type,
+                                                                    ServerHttpRequest request) {
+        Mono<ResponseEntity<CategoryPostVo>> getCategoryPost = postHelper.getCategoryPost(moldevId, type, request.getHeaders());
+        Mono<ResponseEntity<UserProfileVo>> userDetailResponseDtoMono = userHelper.getUserProfileVo(moldevId, request.getHeaders());
+        return Mono.zip(getCategoryPost, userDetailResponseDtoMono).flatMap(tuple -> {
+            PostCategoryResponseDto responseDto = PostCategoryResponseDto.of(tuple.getT1().getBody(), tuple.getT2().getBody());
+            return Mono.just(SuccessResponse.ok(responseDto));
         });
     }
 
-    //게시글 상세조회
-    public Mono<PostDetailTotalResponseDto> getPostInfo(Long postId, Long postWriterId, ServerHttpRequest request) {
-        //게시글 정보 불러옴
-        Mono<PostDetailResponseDto> postDetailInfoMono = postHelper.getPostDetailInfoResult(postId, request.getHeaders());
-        //게시글 작성자 불러옴
-        Mono<UserDetailResponseDto> postWriterDetailInfoMono = userHelper.getUserDetailInfoResult(postWriterId, request.getHeaders());
-        //게시글 달린 댓글 개수 불러옴
-        Mono<CommentCountResponseDto> commentCountMono = replyHelper.getPostCommentCount(postId, request.getHeaders());
-        return Mono.zip(postDetailInfoMono, postWriterDetailInfoMono, commentCountMono).flatMap(tuple ->
-                Mono.just(PostDetailTotalResponseDto.of(tuple.getT1(), tuple.getT2(), tuple.getT3()))
-        );
-
+    public Mono<ResponseEntity<SuccessResponse<?>>> getSearchPosts(String text,
+                                                                   ServerHttpRequest request) {
+        Mono<ResponseEntity<PostDetailListResponseDto>> searchPostVoMono = postHelper.getSearchPostResult(text, request.getHeaders());
+        return searchPostVoMono.flatMap(result -> {
+            List<Long> requestMembers = getRequestMembers(result.getBody().postList());
+            Mono<ResponseEntity<UserDetailListResponseDto>> searchUserVoMono = userHelper.getSearchUserResult(requestMembers, request.getHeaders());
+            return searchUserVoMono.map(nextResult -> {
+                SearchPostsResponseDto responseDto = SearchPostsResponseDto.of(result.getBody(), nextResult.getBody());
+                return SuccessResponse.ok(responseDto);
+            });
+        });
     }
 
+    public Mono<ResponseEntity<SuccessResponse<?>>> getPostInfo(Long postId, Long postWriterId, ServerHttpRequest request) {
+        Mono<ResponseEntity<PostDetailResponseDto>> postDetailInfoMono = postHelper.getPostDetailInfoResult(postId, request.getHeaders());
+        Mono<ResponseEntity<UserDetailResponseDto>> postWriterDetailInfoMono = userHelper.getUserDetailInfoResult(postWriterId, request.getHeaders());
+        Mono<ResponseEntity<CommentCountResponseDto>> commentCountMono = replyHelper.getPostCommentCount(postId, request.getHeaders());
+        return Mono.zip(postDetailInfoMono, postWriterDetailInfoMono, commentCountMono).flatMap(tuple -> {
+            PostDetailTotalResponseDto responseDto = PostDetailTotalResponseDto.of(tuple.getT1().getBody(), tuple.getT2().getBody(), tuple.getT3().getBody());
+            return Mono.just(SuccessResponse.ok(responseDto));
+        });
+    }
+
+    private List<Long> getRequestMembers(List<SearchPostVo> postList) {
+        return postList.stream()
+                .map(SearchPostVo::memberId)
+                .collect(Collectors.toList());
+    }
 }
