1aa4386ad905681f78590f929a1db74557c0711c
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-18 01:15:56 +0900

    KBB-68 [Feat] 응답값 Success Response entity 적용

--- src/main/java/org/bootstrap/apicomposer/domain/reply/controller/ReplyController.java
+++ src/main/java/org/bootstrap/apicomposer/domain/reply/controller/ReplyController.java
@@ -2,9 +2,9 @@ package org.bootstrap.apicomposer.domain.reply.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.bootstrap.apicomposer.domain.reply.dto.response.CommentTotalResponseDto;
-import org.bootstrap.apicomposer.domain.reply.dto.response.ReplyTotalResponseDto;
 import org.bootstrap.apicomposer.domain.reply.service.ReplyService;
+import org.bootstrap.apicomposer.global.common.SuccessResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -13,25 +13,16 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/compose/reply")
 @RestController
 public class ReplyController {
-
     private final ReplyService replyService;
 
-    /**
-     * 댓글 정보 불러오기
-     * 댓글 서비스 댓글 정보 불러오고 -> 댓글 단 유저 정보 가져옴
-     */
     @GetMapping("/{id}")
-    public Mono<CommentTotalResponseDto> getCommentList(@PathVariable("id") final Long postId,
-                                                        ServerHttpRequest request) {
+    public Mono<ResponseEntity<SuccessResponse<?>>> getCommentList(@PathVariable("id") final Long postId,
+                                                                   ServerHttpRequest request) {
         return replyService.getCommentList(postId, request);
     }
 
-    /**
-     * 답글 정보 불러오기
-     * 답글 서비스 답글 정보 불러오고 -> 답글 단 유저 정보 가져옴
-     */
     @GetMapping("")
-    public Mono<ReplyTotalResponseDto> getCommentList(@RequestParam final String parentsId,
+    public Mono<ResponseEntity<SuccessResponse<?>>> getCommentList(@RequestParam final String parentsId,
                                                       ServerHttpRequest request) {
         return replyService.getReplyList(parentsId, request);
     }
