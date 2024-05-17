1a5b73b6fd4ad02434858a08421efa8a893e14ae
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-18 01:16:09 +0900

    KBB-68 [Feat] 응답값 Success Response entity 적용

--- src/main/java/org/bootstrap/apicomposer/domain/reply/helper/ReplyHelper.java
+++ src/main/java/org/bootstrap/apicomposer/domain/reply/helper/ReplyHelper.java
@@ -3,10 +3,10 @@ package org.bootstrap.apicomposer.domain.reply.helper;
 import lombok.RequiredArgsConstructor;
 import org.bootstrap.apicomposer.domain.reply.dto.response.CommentCountResponseDto;
 import org.bootstrap.apicomposer.domain.reply.dto.response.CommentDetailListResponseDto;
-//import org.bootstrap.apicomposer.domain.reply.dto.response.ReplyDetailListResponseDto;
 import org.bootstrap.apicomposer.domain.reply.dto.response.ReplyDetailListResponseDto;
 import org.bootstrap.apicomposer.global.webclient.WebClientUtil;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -15,21 +15,23 @@ import static org.bootstrap.apicomposer.global.common.Constants.REPLY_SERVICE_UR
 @RequiredArgsConstructor
 @Component
 public class ReplyHelper {
-
     private final WebClientUtil webClientUtil;
 
-    // 게시글 댓글 정보 가져오기
-    public Mono<CommentDetailListResponseDto> getPostCommentResult(Long postId, HttpHeaders headers) {
-        return webClientUtil.api(REPLY_SERVICE_URL + "/api/reply/"+postId, headers, CommentDetailListResponseDto.class);
+    public Mono<ResponseEntity<CommentDetailListResponseDto>> getPostCommentResult(Long postId, HttpHeaders headers) {
+        return webClientUtil.api(REPLY_SERVICE_URL + "/api/reply/" + postId,
+                headers,
+                CommentDetailListResponseDto.class);
     }
 
-    // 답글 정보 가져오기
-    public Mono<ReplyDetailListResponseDto> getCommentReplyResult(String commentId, HttpHeaders headers) {
-        return webClientUtil.api(REPLY_SERVICE_URL + "/api/reply", headers, ReplyDetailListResponseDto.class);
+    public Mono<ResponseEntity<ReplyDetailListResponseDto>> getCommentReplyResult(String commentId, HttpHeaders headers) {
+        return webClientUtil.api(REPLY_SERVICE_URL + "/api/reply",
+                headers,
+                ReplyDetailListResponseDto.class);
     }
 
-    // 게시글 댓글 개수 가져오기
-    public Mono<CommentCountResponseDto> getPostCommentCount(Long postId, HttpHeaders headers) {
-        return webClientUtil.api(REPLY_SERVICE_URL + "/api/reply/count/"+postId, headers, CommentCountResponseDto.class);
+    public Mono<ResponseEntity<CommentCountResponseDto>> getPostCommentCount(Long postId, HttpHeaders headers) {
+        return webClientUtil.api(REPLY_SERVICE_URL + "/api/reply/count/" + postId,
+                headers,
+                CommentCountResponseDto.class);
     }
 }
