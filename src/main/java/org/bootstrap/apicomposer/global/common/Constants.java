02a9458ead2a1aa283a86b7c4aa67cdba1804566
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-18 00:57:37 +0900

    KBB-68 [Feat] user service url 변경

--- src/main/java/org/bootstrap/apicomposer/global/common/Constants.java
+++ src/main/java/org/bootstrap/apicomposer/global/common/Constants.java
@@ -2,7 +2,7 @@ package org.bootstrap.apicomposer.global.common;
 
 public class Constants {
     public static final String POST_SERVICE_URL = "http://172.16.214.36:30001";
-    public static final String USER_SERVICE_URL = "http://localhost:8082";
+    public static final String USER_SERVICE_URL = "http://172.16.214.36:30009";
     public static final String REPLY_SERVICE_URL = "http://localhost:8083";
     public static final String SEARCH_SERVICE_URL = "http://localhost:8084";
     public static final String AUTH_SERVICE_URL = "http://localhost:8085";
