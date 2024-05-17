bdfed908ea7fb1d3325c9e15414b3fcab93582fc
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-18 00:58:44 +0900

    KBB-68 [Feat] response 응답 type SuccessResponse 적용

--- src/main/java/org/bootstrap/apicomposer/domain/post/contoller/PostController.java
+++ src/main/java/org/bootstrap/apicomposer/domain/post/contoller/PostController.java
@@ -1,11 +1,10 @@
 package org.bootstrap.apicomposer.domain.post.contoller;
 
 import lombok.RequiredArgsConstructor;
-import org.bootstrap.apicomposer.domain.post.dto.response.PostCategoryResponseDto;
-import org.bootstrap.apicomposer.domain.post.dto.response.PostDetailTotalResponseDto;
-import org.bootstrap.apicomposer.domain.post.dto.response.SearchPostsResponseDto;
 import org.bootstrap.apicomposer.domain.post.service.PostService;
 import org.bootstrap.apicomposer.domain.post.type.CategoryType;
+import org.bootstrap.apicomposer.global.common.SuccessResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -17,23 +16,22 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/search")
-    public Mono<SearchPostsResponseDto> getSearchPosts(@RequestParam final String text,
-                                                       ServerHttpRequest request) {
+    public Mono<ResponseEntity<SuccessResponse<?>>> getSearchPosts(@RequestParam final String text,
+                                                                   ServerHttpRequest request) {
         return postService.getSearchPosts(text, request);
     }
 
-    // 게시글 상세 조회
     @GetMapping("/{id}")
-    public Mono<PostDetailTotalResponseDto> getPostInfo(@PathVariable("id") final Long postId,
-                                                        @RequestParam final Long postWriterId,
-                                                        ServerHttpRequest request) {
+    public Mono<ResponseEntity<SuccessResponse<?>>> getPostInfo(@PathVariable("id") final Long postId,
+                                                                @RequestParam final Long postWriterId,
+                                                                ServerHttpRequest request) {
         return postService.getPostInfo(postId, postWriterId, request);
     }
 
     @GetMapping("/{moldevId}/category")
-    public Mono<PostCategoryResponseDto> getCategoryPost(@PathVariable final String moldevId,
-                                                         @RequestParam final CategoryType type,
-                                                         ServerHttpRequest request) {
+    public Mono<ResponseEntity<SuccessResponse<?>>> getCategoryPost(@PathVariable final String moldevId,
+                                                                    @RequestParam final CategoryType type,
+                                                                    ServerHttpRequest request) {
         return postService.getCategoryPost(moldevId, type, request);
     }
 }
