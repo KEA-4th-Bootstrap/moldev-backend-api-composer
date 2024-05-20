3dc388593ebfed266b1585641ae8455f14f4cbbb
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-20 14:58:55 +0900

    fix: 서비스 url 상수 수정

--- src/main/java/org/bootstrap/apicomposer/global/common/Constants.java
+++ src/main/java/org/bootstrap/apicomposer/global/common/Constants.java
@@ -1,12 +1,12 @@
 package org.bootstrap.apicomposer.global.common;
 
 public class Constants {
-    public static final String POST_SERVICE_URL = "http://172.16.214.36:30001";
-    public static final String USER_SERVICE_URL = "http://172.16.214.36:30009";
-    public static final String REPLY_SERVICE_URL = "http://localhost:8083";
-    public static final String SEARCH_SERVICE_URL = "http://localhost:8084";
-    public static final String AUTH_SERVICE_URL = "http://localhost:8085";
-    public static final String REPORT_SERVICE_URL = "http://localhost:8086";
-    public static final String CHATBOT_SERVICE_URL = "http://localhost:8087";
-    public static final String RECOMMEND_SERVICE_URL = "http://localhost:8088";
+    public static final String POST_SERVICE_URL = "http://post-service.backend.svc.cluster.local:8080";
+    public static final String USER_SERVICE_URL = "http://user-service.backend.svc.cluster.local:8080";
+    public static final String REPLY_SERVICE_URL = "http://reply-service.backend.svc.cluster.local:8080";
+    public static final String SEARCH_SERVICE_URL = "http://search-service.backend.svc.cluster.local:8080";
+    public static final String AUTH_SERVICE_URL = "http://auth-service.backend.svc.cluster.local:8080";
+    public static final String REPORT_SERVICE_URL = "http://report-service.backend.svc.cluster.local:8080";
+    public static final String CHATBOT_SERVICE_URL = "http://chatbot-service.backend.svc.cluster.local:8080";
+    public static final String RECOMMEND_SERVICE_URL = "http://recommend-service.backend.svc.cluster.local:8080";
 }
