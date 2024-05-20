f91214f611796692faa5e2b82655e167a1cd5122
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-20 15:58:28 +0900

    fix: 서비스 url 상수 수정

--- src/main/java/org/bootstrap/apicomposer/global/common/Constants.java
+++ src/main/java/org/bootstrap/apicomposer/global/common/Constants.java
@@ -1,12 +1,12 @@
 package org.bootstrap.apicomposer.global.common;
 
 public class Constants {
-    public static final String POST_SERVICE_URL = "http://post-service.backend.svc.cluster.local:8080";
-    public static final String MEMBER_SERVICE_URL = "http://member-service.backend.svc.cluster.local:8080";
-    public static final String REPLY_SERVICE_URL = "http://reply-service.backend.svc.cluster.local:8080";
-    public static final String SEARCH_SERVICE_URL = "http://search-service.backend.svc.cluster.local:8080";
-    public static final String AUTH_SERVICE_URL = "http://auth-service.backend.svc.cluster.local:8080";
-    public static final String REPORT_SERVICE_URL = "http://report-service.backend.svc.cluster.local:8080";
-    public static final String CHATBOT_SERVICE_URL = "http://chatbot-service.backend.svc.cluster.local:8080";
-    public static final String RECOMMEND_SERVICE_URL = "http://recommend-service.backend.svc.cluster.local:8080";
+    public static final String POST_SERVICE_URL = "http://post-service.backend.svc.cluster.local:80";
+    public static final String MEMBER_SERVICE_URL = "http://member-service.backend.svc.cluster.local:80";
+    public static final String REPLY_SERVICE_URL = "http://reply-service.backend.svc.cluster.local:80";
+    public static final String SEARCH_SERVICE_URL = "http://search-service.backend.svc.cluster.local:80";
+    public static final String AUTH_SERVICE_URL = "http://auth-service.backend.svc.cluster.local:80";
+    public static final String REPORT_SERVICE_URL = "http://report-service.backend.svc.cluster.local:80";
+    public static final String CHATBOT_SERVICE_URL = "http://chatbot-service.backend.svc.cluster.local:80";
+    public static final String RECOMMEND_SERVICE_URL = "http://recommend-service.backend.svc.cluster.local:80";
 }
