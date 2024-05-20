b468886bb7fbaa5b4cbacd3b164ba3c1549b2cef
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-20 10:45:02 +0900

    fix: build.sh 이그노어처리 및 젠킨스 스크립트 수정

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-java \
--javaagent:elastic-apm-agent.jar \
--Delastic.apm.server_url="$SERVER_URL" \
--Delastic.apm.service_name=api-composer \
--Delastic.apm.secret_token="$SECRET_TOKEN" \
--Delastic.apm.environment=my-environment \
--Delastic.apm.application_packages=org.example \
--jar api-composer.jar
