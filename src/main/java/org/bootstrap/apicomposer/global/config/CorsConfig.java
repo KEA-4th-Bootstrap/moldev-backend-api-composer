3c43d7e4604b56e49a037155e8d92b4655e0dc65
MinchoGreenT <dldmstjq99@gmail.com>
2024-06-05 00:58:39 +0900

    [fix] web.moldev.site cors 핸들링

--- src/main/java/org/bootstrap/apicomposer/global/config/CorsConfig.java
+++ src/main/java/org/bootstrap/apicomposer/global/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000"); // 모든 출처를 허용
+        configuration.addAllowedOrigin("https://web.moldev.site");
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드를 허용
         configuration.addAllowedHeader("*"); // 모든 헤더를 허용
         configuration.setAllowCredentials(true); // 자격 증명 허용
